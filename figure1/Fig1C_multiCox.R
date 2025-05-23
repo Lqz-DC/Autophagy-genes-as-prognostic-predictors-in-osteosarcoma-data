e3bb72751ca3186ad3863ec97a45900c6fa8221a
Lqz-DC <1523946473@qq.com>
2025-05-23 14:07:40 +0800

    Add files via upload

---
+++ figure1/Fig1C_multiCox.R
@@ -0,0 +1,46 @@
+
+library(survival)
+library(forestplot)
+   
+clrs <- fpColors(box="black",line="black", summary="black")            
+rt=read.table("all.txt",header=T,sep="\t",check.names=F,row.names=1)
+
+multiCox=coxph(Surv(futime, fustat) ~ ., data = rt)
+multiCoxSum=summary(multiCox)
+
+outTab=data.frame()
+outTab=cbind(
+             HR=multiCoxSum$conf.int[,"exp(coef)"],
+             HR.95L=multiCoxSum$conf.int[,"lower .95"],
+             HR.95H=multiCoxSum$conf.int[,"upper .95"],
+             pvalue=multiCoxSum$coefficients[,"Pr(>|z|)"])
+outTab=cbind(id=row.names(outTab),outTab)
+write.table(outTab,file="multiCox.xls",sep="\t",row.names=F,quote=F)
+
+
+rt=read.table("multiCox.xls",header=T,sep="\t",row.names=1,check.names=F)
+data=as.matrix(rt)
+HR=data[,1:3]
+hr=sprintf("%.3f",HR[,"HR"])
+hrLow=sprintf("%.3f",HR[,"HR.95L"])
+hrHigh=sprintf("%.3f",HR[,"HR.95H"])
+pVal=data[,"pvalue"]
+pVal=ifelse(pVal<0.001, "<0.001", sprintf("%.3f", pVal))
+tabletext <- 
+  list(c(NA, rownames(HR)),
+       append("pvalue", pVal),
+       append("Hazard ratio",paste0(hr,"(",hrLow,"-",hrHigh,")")) )          
+pdf(file="forest-multicox.pdf",onefile = FALSE,
+       width = 6,             
+       height = 4,           
+       )
+forestplot(tabletext, 
+           rbind(rep(NA, 3), HR),
+           col=clrs,
+           graphwidth=unit(50, "mm"),
+           xlog=T,
+           lwd.ci=2,
+           boxsize=0.3,
+           xlab="Hazard ratio"
+           )
+dev.off()
